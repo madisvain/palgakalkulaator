d179b05aff9e1a852280f4b0fecd3a5a42e52281
Madis Väin <madisvain@gmail.com>
2022-10-16 22:53:21 +0300

    finalize analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,57 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import type { AppProps } from "next/app";
+
+import Head from "next/head";
+import Script from "next/script";
+
+import { GTM_ID, pageview } from "../utils/gtm";
+
+function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on("routeChangeComplete", pageview);
+    return () => {
+      router.events.off("routeChangeComplete", pageview);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Head>
+        <title>Palgakalkulaator</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Arvuta palgakalkulaatoriga maksud ja saada palgalehed."
+        />
+      </Head>
+
+      {/* Google Tag Manager */}
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+      ></Script>
+      <Script
+        id="gtag"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-11W0BWKSY3');
+          `,
+        }}
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default App;
