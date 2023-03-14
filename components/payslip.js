c1fc5783a42f46e274ee5d1b2d535238ca762b5e
Madis Väin <madisvain@gmail.com>
2023-03-14 11:44:39 +0200

    payslip logic

---
+++ components/payslip.js
@@ -0,0 +1,213 @@
+import { Trans } from "@lingui/macro";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+import Link from "next/link";
+
+const Payslip = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {},
+  });
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-row pt-24 pb-28 px-16">
+          <div className="bg-white w-full py-28 px-20">
+            <div className="flex flex-row gap-4">
+              <div className="basis-1/2">
+                <input
+                  {...register("title")}
+                  type="text"
+                  className="border-transparent bg-beige w-full h-12 font-semibold text-3xl"
+                />
+              </div>
+              <div className="basis-1/2">
+                <div className="flex flex-row gap-4 items-center mb-3">
+                  <div className="basis-1/2 text-right text-dark-blue opacity-40 font-general font-semibold">
+                    <Trans>Tööandja</Trans>
+                  </div>
+                  <div className="basis-1/2">
+                    <input
+                      {...register("employer")}
+                      type="text"
+                      className="border-transparent bg-beige w-full h-9"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-row gap-4 items-center mb-3">
+                  <div className="basis-1/2 text-right text-dark-blue opacity-40 font-general font-semibold">
+                    <Trans>Töötaja</Trans>
+                  </div>
+                  <div className="basis-1/2">
+                    <input
+                      {...register("employee")}
+                      type="text"
+                      className="border-transparent bg-beige w-full h-9"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-row gap-4 items-center mb-3">
+                  <div className="basis-1/2 text-right text-dark-blue opacity-40 font-general font-semibold">
+                    <Trans>Isikukood</Trans>
+                  </div>
+                  <div className="basis-1/2">
+                    <input
+                      {...register("personalCode")}
+                      type="text"
+                      className="border-transparent bg-beige w-full h-9"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-row gap-4 items-center">
+                  <div className="basis-1/2 text-right text-dark-blue opacity-40 font-general font-semibold">
+                    <Trans>Kuupäev</Trans>
+                  </div>
+                  <div className="basis-1/2">
+                    <input
+                      {...register("date")}
+                      type="text"
+                      className="border-transparent bg-beige w-full h-9"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-row gap-4 mt-28 mb-2">
+              <div className="basis-1/2 font-semibold leading-relaxed opacity-40 text-sm">
+                Tasu liik*
+              </div>
+              <div className="basis-1/2">
+                <div className="flex flex-row gap-4">
+                  <div className="basis-1/6 font-semibold leading-relaxed opacity-40 text-sm text-right">
+                    Aeg
+                  </div>
+                  <div className="basis-1/6 font-semibold leading-relaxed opacity-40 text-sm text-right">
+                    Ühik
+                  </div>
+                  <div className="basis-2/6 font-semibold leading-relaxed opacity-40 text-sm text-right">
+                    Ühiku hind
+                  </div>
+                  <div className="basis-2/6 font-semibold leading-relaxed opacity-40 text-sm text-right">
+                    Summa*
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row gap-4">
+              <div className="basis-1/2">
+                <input
+                  {...register("personalCode")}
+                  type="text"
+                  className="border-transparent bg-beige w-full h-9"
+                />
+              </div>
+              <div className="basis-1/2">
+                <div className="flex flex-row gap-4">
+                  <div className="basis-1/6">
+                    <input
+                      {...register("personalCode")}
+                      type="text"
+                      className="border-transparent bg-beige w-full h-9"
+                    />
+                  </div>
+                  <div className="basis-1/6">
+                    <input
+                      {...register("personalCode")}
+                      type="text"
+                      className="border-transparent bg-beige w-full h-9"
+                    />
+                  </div>
+                  <div className="basis-2/6">
+                    <input
+                      {...register("personalCode")}
+                      type="text"
+                      className="border-transparent bg-beige w-full h-9"
+                    />
+                  </div>
+                  <div className="basis-2/6">
+                    <input
+                      {...register("personalCode")}
+                      type="text"
+                      className="border-transparent bg-beige w-full h-9"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-row gap-4 mt-24">
+              <div className="basis-1/2"></div>
+              <div className="basis-1/2">
+                <h6 className="font-semibold mb-6">
+                  <Trans>Kinni peetud:</Trans>
+                </h6>
+                <div className="flex flex-row mb-3">
+                  <div className="grow font-semibold leading-relaxed text-base">
+                    <Trans>Kogumispension</Trans>
+                  </div>
+                  <div className="font-semibold leading-relaxed text-base">
+                    20.00 EUR
+                  </div>
+                </div>
+                <div className="flex flex-row mb-3">
+                  <div className="grow font-semibold leading-relaxed text-base">
+                    <Trans>Töötaja töötuskindlustusmakse</Trans>
+                  </div>
+                  <div className="font-semibold leading-relaxed text-base">
+                    16.00 EUR
+                  </div>
+                </div>
+                <div className="flex flex-row mb-3">
+                  <div className="grow font-semibold leading-relaxed text-base">
+                    <Trans>Tulumaks</Trans>
+                  </div>
+                  <div className="font-semibold leading-relaxed text-base">
+                    62.00 EUR
+                  </div>
+                </div>
+                <div className="flex flex-row mb-3">
+                  <div className="grow font-semibold leading-relaxed text-base">
+                    <Trans>Sotsiaalmaks</Trans>
+                  </div>
+                  <div className="font-semibold leading-relaxed text-base">
+                    330.00 EUR
+                  </div>
+                </div>
+                <div className="flex flex-row mb-3">
+                  <div className="grow font-semibold leading-relaxed text-base">
+                    <Trans>Tööandja töötuskindlustusmakse</Trans>
+                  </div>
+                  <div className="font-semibold leading-relaxed text-base">
+                    8.00 EUR
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-between mt-40">
+              <button className="bg-green py-5 px-8 font-semibold h-[66px]">
+                <Trans>Salvesta PDF</Trans>
+              </button>
+              <div className="text-right">
+                <h6 className="font-semibold mb-1">
+                  <Trans>Tasumisele kuuluv töötasu</Trans>
+                </h6>
+                <h3 className="font-semibold">1 000.00 EUR</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Payslip;
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import { Trans } from "@lingui/macro";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { parse, simplify, round } from "mathjs";
 
-import Link from "next/link";
+import Payslip from "../components/payslip";
+import TaxInfo from "../components/tax-info";
 
 const DEFAULT_GROSS_AMOUNT = 1000;
 
@@ -333,120 +334,12 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="bg-white">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="flex flex-row pt-24 pb-32 items-center">
-            <div className="basis-2/5">
-              <img src="./taxes.svg" alt="Taxes" />
-            </div>
-            <div className="basis-3/5">
-              <h4 className="mb-6">Maksuinfo 2023</h4>
-              <ul className="list-outside list-disc space-y-4 ml-4">
-                <li>
-                  Tulumaksu kinnipidamise määr on{" "}
-                  <strong className="font-bold">20%</strong>{" "}
-                  <Link
-                    href="#"
-                    className="font-semibold text-xs underline underline-offset-4"
-                  >
-                    TMS § 4 lg 1
-                  </Link>
-                </li>
-                <li>
-                  Maksuvaba tulu varieeruv 0...7848 / 12 ={" "}
-                  <strong>0...654 EUR/kuus</strong>{" "}
-                  <Link
-                    href="#"
-                    className="font-semibold text-xs underline underline-offset-4"
-                  >
-                    TMS § 23 lg 1 ja lg 2
-                  </Link>
-                </li>
-                <li>
-                  Sotsiaalmaksu määr on 33%{" "}
-                  <Link
-                    href="#"
-                    className="font-semibold text-xs underline underline-offset-4"
-                  >
-                    SMS § 7 lg 1
-                  </Link>
-                </li>
-                <li>
-                  Sotsiaalmaksu kuumäär on <strong>654 EUR</strong>, st
-                  sotsiaalmaksu minimaalne kohustus on 654 x 0.33 ={" "}
-                  <strong>215,82 EUR/kuus</strong>{" "}
-                  <Link
-                    href="#"
-                    className="font-semibold text-xs underline underline-offset-4"
-                  >
-                    2023 RES § 2 lg 7, SMS § 2 lg 2, SMS § 21
-                  </Link>
-                </li>
-                <li>
-                  Töötuskindlustusmakse määrad on:
-                  <ul className="list-outside list-disc ml-8">
-                    <li>
-                      töötajale <strong>1.6%</strong>{" "}
-                      <Link
-                        href="#"
-                        className="font-semibold text-xs underline underline-offset-4"
-                      >
-                        VV määrus 15.09.2022 nr 88
-                      </Link>
-                    </li>
-                    <li>
-                      tööandjale <strong>0.8%</strong>{" "}
-                      <Link
-                        href="#"
-                        className="font-semibold text-xs underline underline-offset-4"
-                      >
-                        VV määrus 15.09.2022 nr 88
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  Kogumispensioni makse määr on <strong>liitunutele 2%</strong>{" "}
-                  ja <strong>mitteliitunutele 0%</strong>. Kogumispensioniga
-                  liitumist saab kontrollida{" "}
-                  <Link href="#" className="underline underline-offset-4">
-                    <strong>siit</strong>
-                  </Link>{" "}
-                  <Link
-                    href="#"
-                    className="font-semibold text-xs underline underline-offset-4"
-                  >
-                    KPS § 9
-                  </Link>
-                </li>
-                <li>
-                  Töötasu alammäär (miinimumpalk) on:
-                  <ul className="list-outside list-disc ml-8">
-                    <li>
-                      kuus <strong>725 EUR</strong>{" "}
-                      <Link
-                        href="#"
-                        className="font-semibold text-xs underline underline-offset-4"
-                      >
-                        VV määrus 09.12.2022 nr 124
-                      </Link>
-                    </li>
-                    <li>
-                      tunnis <strong>4.3 EUR</strong>{" "}
-                      <Link
-                        href="#"
-                        className="font-semibold text-xs underline underline-offset-4"
-                      >
-                        VV määrus 09.12.2022 nr 124
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+
+      <div className="bg-rainbow">
+        <Payslip />
       </div>
+
+      <TaxInfo />
     </>
   );
 };
