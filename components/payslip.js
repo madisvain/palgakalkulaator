3fe10680f6c086d4727d7e83013143dc357c9fdf
Madis Väin <madisvain@gmail.com>
2023-07-18 23:15:03 +0300

    initial payslip implementation

--- components/payslip.js
+++ components/payslip.js
@@ -1,7 +1,12 @@
 import { Trans } from "@lingui/macro";
 import { useForm, SubmitHandler } from "react-hook-form";
 
+import dayjs from "dayjs";
 import Link from "next/link";
+
+import "dayjs/locale/et";
+
+dayjs.locale("et");
 
 const Payslip = () => {
   const {
@@ -10,7 +15,9 @@ const Payslip = () => {
     watch,
     formState: { errors },
   } = useForm({
-    defaultValues: {},
+    defaultValues: {
+      title: `Palgaleht ${dayjs().subtract(1, "month").format("MMMM YYYY")}`,
+    },
   });
 
   const onSubmit = (data) => console.log(data);
@@ -20,7 +27,7 @@ const Payslip = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-row pt-24 pb-28 px-16">
           <div className="bg-white w-full py-28 px-20">
-            <div className="flex flex-row gap-4">
+            <div className="flex flex-row gap-8 items-center mb-12">
               <div className="basis-1/2">
                 <input
                   {...register("title")}
@@ -28,6 +35,9 @@ const Payslip = () => {
                   className="border-transparent bg-beige w-full h-12 font-semibold text-3xl"
                 />
               </div>
+              <div className="basis-1/2"></div>
+            </div>
+            <div className="flex flex-row gap-8">
               <div className="basis-1/2">
                 <div className="flex flex-row gap-4 items-center mb-3">
                   <div className="basis-1/2 text-right text-dark-blue opacity-40 font-general font-semibold">
@@ -78,8 +88,54 @@ const Payslip = () => {
                   </div>
                 </div>
               </div>
+              <div className="basis-1/2">
+                <h6 className="font-semibold mb-6">
+                  <Trans>Kinni peetud:</Trans>
+                </h6>
+                <div className="flex flex-row mb-3">
+                  <div className="grow font-semibold leading-relaxed text-base">
+                    <Trans>Kogumispension</Trans>
+                  </div>
+                  <div className="font-semibold leading-relaxed text-base">
+                    20.00 EUR
+                  </div>
+                </div>
+                <div className="flex flex-row mb-3">
+                  <div className="grow font-semibold leading-relaxed text-base">
+                    <Trans>Töötaja töötuskindlustusmakse</Trans>
+                  </div>
+                  <div className="font-semibold leading-relaxed text-base">
+                    16.00 EUR
+                  </div>
+                </div>
+                <div className="flex flex-row mb-3">
+                  <div className="grow font-semibold leading-relaxed text-base">
+                    <Trans>Tulumaks</Trans>
+                  </div>
+                  <div className="font-semibold leading-relaxed text-base">
+                    62.00 EUR
+                  </div>
+                </div>
+                <div className="flex flex-row mb-3">
+                  <div className="grow font-semibold leading-relaxed text-base">
+                    <Trans>Sotsiaalmaks</Trans>
+                  </div>
+                  <div className="font-semibold leading-relaxed text-base">
+                    330.00 EUR
+                  </div>
+                </div>
+                <div className="flex flex-row mb-3">
+                  <div className="grow font-semibold leading-relaxed text-base">
+                    <Trans>Tööandja töötuskindlustusmakse</Trans>
+                  </div>
+                  <div className="font-semibold leading-relaxed text-base">
+                    8.00 EUR
+                  </div>
+                </div>
+              </div>
             </div>
 
+            {/* Payslip rows
             <div className="flex flex-row gap-4 mt-28 mb-2">
               <div className="basis-1/2 font-semibold leading-relaxed opacity-40 text-sm">
                 Tasu liik*
@@ -142,55 +198,7 @@ const Payslip = () => {
                 </div>
               </div>
             </div>
-
-            <div className="flex flex-row gap-4 mt-24">
-              <div className="basis-1/2"></div>
-              <div className="basis-1/2">
-                <h6 className="font-semibold mb-6">
-                  <Trans>Kinni peetud:</Trans>
-                </h6>
-                <div className="flex flex-row mb-3">
-                  <div className="grow font-semibold leading-relaxed text-base">
-                    <Trans>Kogumispension</Trans>
-                  </div>
-                  <div className="font-semibold leading-relaxed text-base">
-                    20.00 EUR
-                  </div>
-                </div>
-                <div className="flex flex-row mb-3">
-                  <div className="grow font-semibold leading-relaxed text-base">
-                    <Trans>Töötaja töötuskindlustusmakse</Trans>
-                  </div>
-                  <div className="font-semibold leading-relaxed text-base">
-                    16.00 EUR
-                  </div>
-                </div>
-                <div className="flex flex-row mb-3">
-                  <div className="grow font-semibold leading-relaxed text-base">
-                    <Trans>Tulumaks</Trans>
-                  </div>
-                  <div className="font-semibold leading-relaxed text-base">
-                    62.00 EUR
-                  </div>
-                </div>
-                <div className="flex flex-row mb-3">
-                  <div className="grow font-semibold leading-relaxed text-base">
-                    <Trans>Sotsiaalmaks</Trans>
-                  </div>
-                  <div className="font-semibold leading-relaxed text-base">
-                    330.00 EUR
-                  </div>
-                </div>
-                <div className="flex flex-row mb-3">
-                  <div className="grow font-semibold leading-relaxed text-base">
-                    <Trans>Tööandja töötuskindlustusmakse</Trans>
-                  </div>
-                  <div className="font-semibold leading-relaxed text-base">
-                    8.00 EUR
-                  </div>
-                </div>
-              </div>
-            </div>
+            */}
 
             <div className="flex justify-between mt-40">
               <button className="bg-green py-5 px-8 font-semibold h-[66px]">
@@ -198,7 +206,7 @@ const Payslip = () => {
               </button>
               <div className="text-right">
                 <h6 className="font-semibold mb-1">
-                  <Trans>Tasumisele kuuluv töötasu</Trans>
+                  <Trans>Tasumisele kuuluv NETO töötasu</Trans>
                 </h6>
                 <h3 className="font-semibold">1 000.00 EUR</h3>
               </div>
--- pages/index.js
+++ pages/index.js
@@ -193,6 +193,8 @@ const Home = () => {
     { id: "net", title: "Netopalk" },
   ];
 
+  const [showPayslip, setShowPayslip] = useState(false);
+
   return (
     <>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -415,33 +417,38 @@ const Home = () => {
                 </tbody>
               </table>
             </div>
-            <button
-              type="button"
-              className="flex justify-center items-center w-full h-[66px] bg-green font-semibold"
-            >
-              Koosta palgateatis
-              <div className="inline ml-2">
-                <svg
-                  width="12"
-                  height="8"
-                  viewBox="0 0 12 8"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M10.293 0.292969L5.99997 4.58597L1.70697 0.292969L0.292969 1.70697L5.99997 7.41397L11.707 1.70697L10.293 0.292969Z"
-                    fill="#181A33"
-                  />
-                </svg>
-              </div>
-            </button>
+            {!showPayslip && (
+              <button
+                type="button"
+                className="flex justify-center items-center w-full h-[66px] bg-green font-semibold"
+                onClick={() => setShowPayslip(true)}
+              >
+                <Trans>Koosta palgateatis</Trans>
+                <div className="inline ml-2">
+                  <svg
+                    width="12"
+                    height="8"
+                    viewBox="0 0 12 8"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M10.293 0.292969L5.99997 4.58597L1.70697 0.292969L0.292969 1.70697L5.99997 7.41397L11.707 1.70697L10.293 0.292969Z"
+                      fill="#181A33"
+                    />
+                  </svg>
+                </div>
+              </button>
+            )}
           </div>
         </div>
       </div>
 
-      <div className="bg-rainbow">
-        <Payslip />
-      </div>
+      {showPayslip && (
+        <div className="bg-rainbow">
+          <Payslip />
+        </div>
+      )}
 
       <TaxInfo />
     </>
