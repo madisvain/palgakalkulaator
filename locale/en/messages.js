58cc2a652dd49ec032178e80e325f8c72f9ec7f0
Madis Väin <madisvain@gmail.com>
2023-07-24 00:09:41 +0300

    compile messages

---
+++ locale/en/messages.js
@@ -0,0 +1,1 @@
+/*eslint-disable*/module.exports={messages:JSON.parse("{\"Arvuta palgakalkulaatoriga maksud ja saada palgalehed.\":\"Arvuta palgakalkulaatoriga maksud ja saada palgalehed.\",\"Avame varsti\":\"Avame varsti\",\"Kontakt\":\"Contact\",\"Palgakalkulaator\":\"Palgakalkulaator\",\"Seadusemuudatused\":\"Seadusemuudatused\",\"Teenused\":\"Teenused\"}")};
---
+++ locale/et/messages.js
@@ -0,0 +1,1 @@
+/*eslint-disable*/module.exports={messages:JSON.parse("{\"Arvuta palgakalkulaatoriga maksud ja saada palgalehed.\":\"Arvuta palgakalkulaatoriga maksud ja saada palgalehed.\",\"Avame varsti\":\"Avame varsti\",\"Kontakt\":\"Kontakt\",\"Palgakalkulaator\":\"Palgakalkulaator\",\"Seadusemuudatused\":\"Seadusemuudatused\",\"Teenused\":\"Teenused\"}")};
