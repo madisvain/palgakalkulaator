9e5864c93b1ae7f47569b8e1c1e58a31ba70a749
Madis Väin <madisvain@gmail.com>
2023-07-30 21:27:41 +0300

    add README and final cleanup

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
-* Sotsiaalmaksu miiniumum arvutus tööandja kulu kaudu
-* Palgalehele märkuse info
-* Palgalehele töötaja, tööandja jne andmed
-* Mobiili vaated
+# 🧮 Palgakalkulaator
 
-* Arveldaja lehel kaasajastada tõlkesüsteem
+[palgakalkulaator.ee](https://www.palgakalkulaator.ee)
+
+Palgakalkulaator aitab arvestada netopalga, brutopalga, tööandja kulu, maksud ja luua töötajale palgalehe.
+
+![Palgakalkulaator](https://www.palgakalkulaator.ee/taxes.svg)
+
+## Võimalused
+
+- Arvutuse aluseks oleva summa liigi valik (tööandja kulu, brotopalk, netopalk)
+- Sotsiaalmaksu minimaalse kuumäära arvestamine
+- Maksuvaba tulu arvestamine (muudetav)
+- Tööandja töötuskindlustusmakse
+- Töötaja töötuskindlustusmakse
+- Kogumispensioniga liitumise arvestus
+- Palgateatise koostamine (pdf formaadis)
+
+## Tarkvararaamistikud
+- Vercel paigaldus
+- Next.js (React.js) raamistik
+- Tailwind CSS raamistik
+- Lingui.js tõlgeteks
+- Math.js
+- Sentry vigade monitoorimiseks
+- Cypress automaattestid
+- React PDF palgalehe genereerimiseks
+
+## Litsents
+
+[GNU Affero General Public License v3 (AGPL-3.0)](https://www.tldrlegal.com/license/gnu-affero-general-public-license-v3-agpl-3-0)
--- components/layout.js
+++ components/layout.js
@@ -1,24 +1,10 @@
-import React, { useEffect } from "react";
 import Head from "next/head";
-import Script from "next/script";
-import { useRouter } from "next/router";
 import { Trans } from "@lingui/macro";
 
 import Navigation from "./navigation";
 import Footer from "./footer";
-import { GTM_ID, pageview } from "../utils/gtm";
 
 const Layout = ({ children }) => {
-  const router = useRouter();
-
-  // Google analytics
-  useEffect(() => {
-    router.events.on("routeChangeComplete", pageview);
-    return () => {
-      router.events.off("routeChangeComplete", pageview);
-    };
-  }, [router.events]);
-
   return (
     <>
       <Head>
@@ -34,27 +20,6 @@ const Layout = ({ children }) => {
           }
         />
       </Head>
-
-      {/* Google Tag Manager */}
-      <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
-      ></Script>
-      <Script
-        id="gtag"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-11W0BWKSY3', {
-            page_path: window.location.pathname,
-          });
-          `,
-        }}
-      />
 
       <Navigation />
 
