7a69e5682d68aa1aab9f709058b6ba4af1dfc4cf
Madis Väin <madisvain@gmail.com>
2024-02-07 16:50:39 +0200

    add simpleanalytics

--- pages/_app.js
+++ pages/_app.js
@@ -70,6 +70,15 @@ function App({ Component, pageProps }) {
           },
         }}
       />
+      {/* Simple Analytics */}
+      <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+      <noscript>
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerpolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
     </div>
   );
 }
