fa8f6fb81b2afe1c1910f64b6bcc63b9c9971110
Madis Väin <madisvain@gmail.com>
2024-09-07 10:19:15 +0300

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,9 @@
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div className="w-full mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="flex flex-col mt-12 mb-14 space-y-12 md:flex-row lg:mt-24 lg:mb-28 md:space-x-4 md:space-y-0">
+        <h1 className="text-center w-full">🚨 404 - Page Not Found</h1>
+      </div>
+    </div>
+  );
 }
